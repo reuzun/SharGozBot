5be863d4c3d337213c05df51105da9636ad6cb09
reuzun <moruka12@yandex.com>
2021-01-23 20:16:35 +0300

    Bug fixes.

--- src/main/java/ceng/estu/DMusic.java
+++ src/main/java/ceng/estu/DMusic.java
@@ -4,6 +4,7 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
@@ -14,10 +15,8 @@ import discord4j.core.object.entity.channel.VoiceChannel;
 import discord4j.voice.AudioProvider;
 
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.sql.SQLOutput;
+import java.util.*;
 
 /**
  * @author reuzun
@@ -26,7 +25,7 @@ public class DMusic {
 
     private static final Map<String, Command> commands = new HashMap<>();
     private static String SYSTEM_PREFIX_PROPERTY = "!";
-
+static boolean bool = true;
     static {
         commands.put("ping", event -> event.getMessage()
                 .getChannel().block()
@@ -67,29 +66,87 @@ public class DMusic {
 
         final TrackScheduler scheduler = new TrackScheduler(player);
         commands.put("play", event -> {
+            final Member member = event.getMember().orElse(null);
+            if (member != null) {
+                final VoiceState voiceState = member.getVoiceState().block();
+                if (voiceState != null) {
+                    final VoiceChannel channel = voiceState.getChannel().block();
+                    if (channel != null) {
+                        // join returns a VoiceConnection which would be required if we were
+                        // adding disconnection features, but for now we are just ignoring it.
+                        channel.join(spec -> spec.setProvider(provider)).block();
+                    }
+                }
+            }
+
             final String content = event.getMessage().getContent();
             final List<String> command = Arrays.asList(content.split(" "));
-            if(isLink(command.get(1)) && command.size() == 2){
+            if (isLink(command.get(1)) && command.size() == 2) {
                 playerManager.loadItem(command.get(1), scheduler);
-            }else{
-                playerManager.loadItem("ytsearch: " + content.replace(SYSTEM_PREFIX_PROPERTY+"play",""), scheduler);
+            } else {
+                playerManager.loadItem("ytsearch: " + content.replace(SYSTEM_PREFIX_PROPERTY + "play", ""), scheduler);
             }
-            event.getMessage().getChannel().block().createMessage("Song Name : "+TrackScheduler.player.getPlayingTrack().getInfo().title + "\n" +
-                    "Author : " + TrackScheduler.player.getPlayingTrack().getInfo().author);
+            try {
+                Thread.sleep(1600);
+            } catch (Exception e) {
+            }
+
+            if (!bool) {
+                try {
+                    event.getMessage().getChannel().block().createMessage(":\nPlaying song is : " + TrackScheduler.audioPlayStack.peek().getInfo().title +
+                            "\nAuthor of Song : " + TrackScheduler.audioPlayStack.peek().getInfo().author +
+                            "\nDuration of Song : " + TrackScheduler.audioPlayStack.peek().getInfo().length + "ms" +
+                            "\nLink of Song : " + TrackScheduler.audioPlayStack.peek().getInfo().uri).block();
+                }catch (EmptyStackException exe){
+                    event.getMessage().getChannel().block().createMessage(":\nWe believe that number 2 " +
+                            "is an unlucky number. So we do" +
+                            " not provide information about that" +
+                            "song.").block();
+                }
+            }
+            else{
+                event.getMessage().getChannel().block().createMessage(":\nPlaying song is : " + TrackScheduler.player.getPlayingTrack().getInfo().title +
+                        "\nAuthor of Song : " + TrackScheduler.player.getPlayingTrack().getInfo().author +
+                        "\nDuration of Song : " + TrackScheduler.player.getPlayingTrack().getInfo().length + "ms" +
+                        "\nLink of Song : " + TrackScheduler.player.getPlayingTrack().getInfo().uri).block();
+                bool = false;
+            }
+
         });
         commands.put("pause", event -> {
             player.setPaused(true);
+            event.getMessage().getChannel().block().createMessage("Paused...").block();
+        });
+        commands.put("stop", event -> {
+            event.getMessage().getChannel().block().createMessage("Paused...").block();
         });
         commands.put("skip", event -> {
-            player.setPaused(true);
+            if(!TrackScheduler.audioPlayStack.isEmpty()) {
+                TrackScheduler.player.playTrack(TrackScheduler.audioPlayStack.pop());
+                event.getMessage().getChannel().block().createMessage("Next song will be playing ASAP.").block();
+            }else
+                event.getMessage().getChannel().block().createMessage("No Song is available.").block();
         });
         commands.put("cont", event -> {
             player.setPaused(false);
+            event.getMessage().getChannel().block().createMessage("Playing...").block();
         });
         commands.put("setvol", event -> {
-            player.setVolume(Integer.parseInt(event.getMessage().getContent().substring(event.getMessage().getContent().lastIndexOf(" "))));
+            player.setVolume(Integer.parseInt(event.getMessage().getContent().substring(event.getMessage().getContent().lastIndexOf(" "+1))));
+            event.getMessage().getChannel().block().createMessage("Volume is set to : " + player.getVolume()).block();
         });
-
+        commands.put("mov", event -> {
+           player.getPlayingTrack().setPosition(Integer.parseInt(event.getMessage().getContent().substring(event.getMessage().getContent().lastIndexOf(" "+1))));
+            event.getMessage().getChannel().block().createMessage("Seeked to : " + player.getPlayingTrack().getPosition()).block();
+        });
+        commands.put("li", event -> {
+            int i = 0;
+            if(!((Stack<AudioTrack>)TrackScheduler.audioPlayStack.clone()).isEmpty())
+            for (AudioTrack t : (Stack<AudioTrack>)TrackScheduler.audioPlayStack.clone())
+                event.getMessage().getChannel().block().createMessage("\n" + (i++) + ". song : " + t.getInfo().title + " Duration : " + t.getInfo().length + "ms").block();
+            else
+                event.getMessage().getChannel().block().createMessage("No song Left." + player.getVolume()).block();
+        });
 
 
         final GatewayDiscordClient client = DiscordClientBuilder.create(args[0]).build()
@@ -115,8 +172,8 @@ public class DMusic {
         client.onDisconnect().block();
     }
 
-    private static boolean isLink(String str){
-        return str.substring(0, 3).equals("htt");
+    private static boolean isLink(String str) {
+        return str.substring(0,4).equals("http");
     }
 
 }
--- src/main/java/ceng/estu/TrackScheduler.java
+++ src/main/java/ceng/estu/TrackScheduler.java
@@ -2,65 +2,80 @@ package ceng.estu;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.event.AudioEvent;
 import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
+import com.sedmelluq.discord.lavaplayer.player.event.AudioEventListener;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Stack;
 
 /**
  * @author reuzun
  */
-public final class TrackScheduler extends AudioEventAdapter implements AudioLoadResultHandler {
+public final class TrackScheduler implements AudioLoadResultHandler {
 
-    static String addedSongName = "";
+    //static ArrayList<AudioTrack> list = new ArrayList<>();
+
     static Stack<AudioTrack> audioPlayStack = new Stack<>();
     public static AudioPlayer player = null;
 
+
     public TrackScheduler(final AudioPlayer player) {
+
         this.player = player;
+
+        player.addListener(new AudioEventListener() {
+            @Override
+            public void onEvent(AudioEvent event) {
+                System.out.println("An event has occured.");
+                if(!audioPlayStack.isEmpty())
+                    trackLoaded(audioPlayStack.pop());
+                else
+                    System.out.println("Stack is emty for listener");
+            }
+        });
     }
 
     @Override
     public void trackLoaded(final AudioTrack track) {
-        System.out.println("trackLoaded");
         // LavaPlayer found an audio source for us to play
+        //list.add(track);
+        //if(player.getPlayingTrack() != null)
+
         if(player.getPlayingTrack() != null){
             audioPlayStack.push(track);
             return;
         }
+
         player.playTrack(track);
-        while(player.getPlayingTrack() != null){
-            try {
-                Thread.sleep(track.getDuration()+500);
-            }catch (Exception e){
-                //do nothing
-                System.out.println("HATA ALDIK GARİPTİR Kİ ");
-            }
-        }
-        System.out.println("ŞARKI BİTTİ");
-        if (!audioPlayStack.isEmpty())
+
+        /*while(player.getPlayingTrack() != null){
+            //do nothing. This for waiting end of song.
+        }*/
+
+        if(!audioPlayStack.isEmpty()) {
             trackLoaded(audioPlayStack.pop());
+        }
     }
 
     @Override
     public void playlistLoaded(final AudioPlaylist playlist) {
         // LavaPlayer found multiple AudioTracks from some playlist
-        System.out.println("PLAYLİSTLOADED");
         trackLoaded(playlist.getTracks().get(0));
-
     }
 
     @Override
     public void noMatches() {
         // LavaPlayer did not find any audio to extract
-        System.out.println("NOMATCHES");
     }
 
     @Override
     public void loadFailed(final FriendlyException exception) {
         // LavaPlayer could not parse an audio source for some reason
-        System.out.println("LOADFAİLED");
     }
 }
